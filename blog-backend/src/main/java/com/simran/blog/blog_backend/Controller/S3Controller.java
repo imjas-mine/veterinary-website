8771e616db54a50a4295666de4c256339e43dcc0
jasmine <jasminkaur5858@gmail.com>
2025-12-29 02:32:13 -0500

    created s3controller providing endpoints for upload and download image

---
+++ blog-backend/src/main/java/com/simran/blog/blog_backend/Controller/S3Controller.java
@@ -0,0 +1,46 @@
+package com.simran.blog.blog_backend.Controller;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.simran.blog.blog_backend.S3.S3Service;
+
+@RestController
+@RequestMapping("/api/s3")
+public class S3Controller {
+
+    private final S3Service s3Service;
+    @Value("${aws.s3.bucketName}")
+    private String bucketName;
+
+    public S3Controller(S3Service s3Service) {
+        this.s3Service = s3Service;
+    }
+
+
+    @PostMapping("/upload")
+    public void uploadFile(@RequestParam("key") String key, @RequestParam("file") MultipartFile file){
+        try {
+            byte[] fileBytes = file.getBytes();
+            s3Service.putObject(bucketName, key, fileBytes);
+            System.out.println("File uploaded successfully");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("File upload failed");
+        }
+    }
+
+    @GetMapping("/download/{key}")
+    public byte[] downloadFile(@PathVariable String key){
+        return s3Service.getObject(bucketName, key);
+    }
+    
+}
